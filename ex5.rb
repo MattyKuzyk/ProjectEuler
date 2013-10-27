9c8127b82c3f0081ac3526ff6681da5e6b769c06
Matt Kuzyk <matthew.kuzyk@gmail.com>
2013-10-27 15:09:10 -0400

    it's working now, should consider using prime factors

--- ex5.rb
+++ ex5.rb
@@ -6,10 +6,10 @@ answer = 0
 while (numberFound == false)
   i = i + 1
 
-  (1..20).each do |num|
+  (2..20).each do |num|
     break unless i % num == 0
     numberFound = true if num == 20
-    answer = i * num
+    answer = i
   end
 end
 
